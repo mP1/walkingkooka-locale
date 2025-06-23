7a12b6bce64694fa2b429cb8d35f81db2a3080d3
Miroslav Pokorny <miroslav.pokorny@gmail.com>
2025-06-23 22:37:02 +1000

    LocaleContext

---
+++ src/main/java/walkingkooka/locale/LocaleContextTesting.java
@@ -0,0 +1,104 @@
+/*
+ * Copyright 2025 Miroslav Pokorny (github.com/mP1)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package walkingkooka.locale;
+
+import walkingkooka.collect.set.Sets;
+import walkingkooka.datetime.DateTimeSymbols;
+import walkingkooka.math.DecimalNumberSymbols;
+
+import java.util.Locale;
+import java.util.Optional;
+import java.util.Set;
+
+public interface LocaleContextTesting extends HasLocaleTesting {
+
+    default void availableLocalesAndCheck(final LocaleContext context,
+                                          final Locale... expected) {
+        this.availableLocalesAndCheck(
+                context,
+                Sets.of(expected)
+        );
+    }
+
+    default void availableLocalesAndCheck(final LocaleContext context,
+                                          final Set<Locale> expected) {
+        this.checkEquals(
+                expected,
+                context.availableLocales(),
+                context::toString
+        );
+    }
+
+    default void dateTimeSymbolsForLocale(final LocaleContext context,
+                                          final Locale locale) {
+        this.dateTimeSymbolsForLocale(
+                context,
+                locale,
+                Optional.empty()
+        );
+    }
+
+    default void dateTimeSymbolsForLocale(final LocaleContext context,
+                                          final Locale locale,
+                                          final DateTimeSymbols expected) {
+        this.dateTimeSymbolsForLocale(
+                context,
+                locale,
+                Optional.of(expected)
+        );
+    }
+
+    default void dateTimeSymbolsForLocale(final LocaleContext context,
+                                          final Locale locale,
+                                          final Optional<DateTimeSymbols> expected) {
+        this.checkEquals(
+                expected,
+                context.decimalNumberSymbolsForLocale(locale),
+                () -> "dateTimeSymbolsForLocale " + locale
+        );
+    }
+
+    default void decimalNumberSymbolsForLocale(final LocaleContext context,
+                                               final Locale locale) {
+        this.decimalNumberSymbolsForLocale(
+                context,
+                locale,
+                Optional.empty()
+        );
+    }
+
+    default void decimalNumberSymbolsForLocale(final LocaleContext context,
+                                               final Locale locale,
+                                               final DecimalNumberSymbols expected) {
+        this.decimalNumberSymbolsForLocale(
+                context,
+                locale,
+                Optional.of(expected)
+        );
+    }
+
+    default void decimalNumberSymbolsForLocale(final LocaleContext context,
+                                               final Locale locale,
+                                               final Optional<DecimalNumberSymbols> expected) {
+        this.checkEquals(
+                expected,
+                context.decimalNumberSymbolsForLocale(locale),
+                () -> "decimalNumberSymbolsForLocale " + locale
+        );
+    }
+}
---
+++ src/main/java/walkingkooka/locale/LocaleContextTesting2.java
@@ -0,0 +1,44 @@
+/*
+ * Copyright 2025 Miroslav Pokorny (github.com/mP1)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package walkingkooka.locale;
+
+import org.junit.jupiter.api.Test;
+import walkingkooka.ContextTesting;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public interface LocaleContextTesting2<C extends LocaleContext> extends LocaleContextTesting, ContextTesting<C> {
+
+    @Test
+    default void testDateTimeSymbolsForLocaleWithNullFails() {
+        assertThrows(
+                NullPointerException.class,
+                () -> this.createContext()
+                        .dateTimeSymbolsForLocale(null)
+        );
+    }
+
+    @Test
+    default void testDecimalNumberSymbolsForLocaleWithNullFails() {
+        assertThrows(
+                NullPointerException.class,
+                () -> this.createContext()
+                        .decimalNumberSymbolsForLocale(null)
+        );
+    }
+}
