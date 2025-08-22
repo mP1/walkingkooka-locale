5a23ecac98b39fe9b4b5f879616fde7b31385c67
Miroslav Pokorny <miroslav.pokorny@gmail.com>
2025-08-22 21:26:30 +1000

    LocaleConverterDecimalNumberSymbols
    - Closes https://github.com/mP1/walkingkooka-locale/issues/43
    - Converter: HasOptionalDecimalNumberSymbols -> DecimalNumberSymbols
    
    - Closes https://github.com/mP1/walkingkooka-locale/issues/42
    - Converter: HasDecimalNumberSymbols -> DecimalNumberSymbols
    
    - Closes https://github.com/mP1/walkingkooka-locale/issues/41
    - Converter: Locale -> DecimalNumberSymbols

---
+++ src/main/java/walkingkooka/locale/convert/LocaleConverterDecimalNumberSymbols.java
@@ -0,0 +1,98 @@
+/*
+ * Copyright 2025 Miroslav Pokorny (github.com/mP1)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package walkingkooka.locale.convert;
+
+import walkingkooka.convert.Converter;
+import walkingkooka.math.DecimalNumberSymbols;
+import walkingkooka.math.HasDecimalNumberSymbols;
+import walkingkooka.math.HasOptionalDecimalNumberSymbols;
+
+import java.util.Locale;
+
+/**
+ * A {@link Converter} that supports several different source types to return a {@link DecimalNumberSymbols}.
+ */
+final class LocaleConverterDecimalNumberSymbols<C extends LocaleConverterContext> extends LocaleConverter<DecimalNumberSymbols, C> {
+
+    /**
+     * Type safe getter.
+     */
+    static <C extends LocaleConverterContext> LocaleConverterDecimalNumberSymbols<C> instance() {
+        return INSTANCE;
+    }
+
+    /**
+     * Singleton
+     */
+    private final static LocaleConverterDecimalNumberSymbols INSTANCE = new LocaleConverterDecimalNumberSymbols();
+
+    private LocaleConverterDecimalNumberSymbols() {
+        super();
+    }
+
+    // LocaleConverter..................................................................................................
+
+    @Override
+    Class<DecimalNumberSymbols> targetType() {
+        return DecimalNumberSymbols.class;
+    }
+
+    @Override
+    boolean canConvertNotString(final Object value,
+                                final C context) {
+        return (value instanceof DecimalNumberSymbols ||
+            value instanceof HasDecimalNumberSymbols ||
+            value instanceof HasOptionalDecimalNumberSymbols);
+    }
+
+    @Override
+    DecimalNumberSymbols tryConvertLocale(final Locale locale,
+                                          final C context) {
+        return context.decimalNumberSymbolsForLocale(locale)
+            .orElse(null);
+    }
+
+    @Override
+    DecimalNumberSymbols tryConvertNonLocale(final Object value,
+                                             final C context) {
+        DecimalNumberSymbols result;
+
+        if (value instanceof DecimalNumberSymbols) {
+            result = (DecimalNumberSymbols) value;
+        } else {
+            if (value instanceof HasDecimalNumberSymbols) {
+                result = ((HasDecimalNumberSymbols) value).decimalNumberSymbols();
+            } else {
+                if (value instanceof HasOptionalDecimalNumberSymbols) {
+                    result = ((HasOptionalDecimalNumberSymbols) value).decimalNumberSymbols()
+                        .orElse(null);
+                } else {
+                    result = this.tryConvertLocale(
+                        context.convertOrFail(
+                            value,
+                            Locale.class
+                        ),
+                        context
+                    );
+                }
+            }
+        }
+
+        return result;
+    }
+}
