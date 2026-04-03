52af30c3ab0d08eded5e878999c0785d85674418
Miroslav Pokorny <miroslav.pokorny@gmail.com>
2026-04-03 18:36:10 +1100

    LocaleLanguageTag
    - Closes https://github.com/mP1/walkingkooka-locale/issues/112
    - LocaleLanguageTag: Copy from walkingkooka.spreadsheet.server.locale.LocaleLanguageTag

---
+++ src/main/java/walkingkooka/locale/LocaleLanguageTag.java
@@ -0,0 +1,97 @@
+/*
+ * Copyright 2025 Miroslav Pokorny (github.com/mP1)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package walkingkooka.locale;
+
+import walkingkooka.Value;
+import walkingkooka.compare.Comparators;
+import walkingkooka.text.CaseSensitivity;
+
+import java.util.Locale;
+import java.util.Objects;
+
+/**
+ * An id that uniquely identifies a {@link Locale} using its language-tag such as EN, EN-AU.
+ */
+public final class LocaleLanguageTag implements Comparable<LocaleLanguageTag>, Value<String> {
+
+    public final static CaseSensitivity CASE_SENSITIVITY = CaseSensitivity.INSENSITIVE;
+
+    public static LocaleLanguageTag fromLocale(final Locale locale) {
+        return new LocaleLanguageTag(
+            Objects.requireNonNull(locale, "locale")
+                .toLanguageTag()
+        );
+    }
+
+    public static LocaleLanguageTag parse(final String languageTag) {
+        return new LocaleLanguageTag(
+            Objects.requireNonNull(languageTag)
+        );
+    }
+
+    private LocaleLanguageTag(final String languageTag) {
+        super();
+
+        this.languageTag = languageTag;
+    }
+
+    // Value............................................................................................................
+
+    @Override
+    public String value() {
+        return this.languageTag;
+    }
+
+    private final String languageTag;
+
+    // Object...........................................................................................................
+
+    @Override
+    public int hashCode() {
+        return CASE_SENSITIVITY.hash(
+            this.languageTag
+        );
+    }
+
+    @Override
+    public boolean equals(final Object other) {
+        return this == other ||
+            (other instanceof LocaleLanguageTag &&
+                this.equals0((LocaleLanguageTag) other));
+    }
+
+    private boolean equals0(final LocaleLanguageTag other) {
+        return this.compareTo(other) == Comparators.EQUAL;
+    }
+
+    @Override
+    public String toString() {
+        return this.languageTag;
+    }
+
+    // Comparable.......................................................................................................
+
+    @Override
+    public int compareTo(final LocaleLanguageTag other) {
+        return CASE_SENSITIVITY.comparator()
+            .compare(
+                this.languageTag,
+                other.languageTag
+            );
+    }
+}
---
+++ src/test/java/walkingkooka/locale/LocaleLanguageTagTest.java
@@ -0,0 +1,120 @@
+/*
+ * Copyright 2025 Miroslav Pokorny (github.com/mP1)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package walkingkooka.locale;
+
+import org.junit.jupiter.api.Test;
+import walkingkooka.compare.ComparableTesting2;
+import walkingkooka.reflect.ClassTesting2;
+import walkingkooka.reflect.JavaVisibility;
+import walkingkooka.test.ParseStringTesting;
+
+import java.util.Locale;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public final class LocaleLanguageTagTest implements ComparableTesting2<LocaleLanguageTag>,
+    ClassTesting2<LocaleLanguageTag>,
+    ParseStringTesting<LocaleLanguageTag> {
+
+    private final static String LOCALE = "en-AU";
+
+    @Test
+    public void testWithNullLocaleFails() {
+        assertThrows(
+            NullPointerException.class,
+            () -> LocaleLanguageTag.parse(null)
+        );
+    }
+
+    @Test
+    public void testWith() {
+        final String languageTag = "en-AU";
+
+        final LocaleLanguageTag localeLanguageTag = LocaleLanguageTag.parse(languageTag);
+        this.checkEquals(
+            languageTag,
+            localeLanguageTag.value()
+        );
+    }
+
+    // parse............................................................................................................
+
+    @Override
+    public void testParseStringEmptyFails() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Test
+    public void testParse() {
+        this.parseStringAndCheck(
+            "en-AU",
+            LocaleLanguageTag.parse(LOCALE)
+        );
+    }
+
+    @Override
+    public LocaleLanguageTag parseString(final String text) {
+        return LocaleLanguageTag.parse(text);
+    }
+
+    @Override
+    public Class<? extends RuntimeException> parseStringFailedExpected(final Class<? extends RuntimeException> thrown) {
+        return thrown;
+    }
+
+    @Override
+    public RuntimeException parseStringFailedExpected(final RuntimeException thrown) {
+        return thrown;
+    }
+
+    // comparable.......................................................................................................
+
+    @Test
+    public void testComparableLess() {
+        this.compareToAndCheckLess(
+            LocaleLanguageTag.parse(
+                Locale.FRANCE.toLanguageTag()
+            )
+        );
+    }
+
+    @Test
+    public void testEqualsDifferentCase() {
+        this.compareToAndCheckEquals(
+            LocaleLanguageTag.parse("en-AU"),
+            LocaleLanguageTag.parse("EN-AU")
+        );
+    }
+
+    @Override
+    public LocaleLanguageTag createComparable() {
+        return LocaleLanguageTag.parse(LOCALE);
+    }
+
+    // class............................................................................................................
+
+    @Override
+    public Class<LocaleLanguageTag> type() {
+        return LocaleLanguageTag.class;
+    }
+
+    @Override
+    public JavaVisibility typeVisibility() {
+        return JavaVisibility.PUBLIC;
+    }
+}
